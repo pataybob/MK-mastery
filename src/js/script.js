6c40cc7dc68d0ee50f7db3f845da0b8eb50e7508
Denis Vishnyackov <Vishka2005@yandex.ru>
2024-06-19 04:25:01 +0400

    f

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,52 @@
-const slider = document.querySelector('.slider');
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
-const slides = Array.from(slider.querySelectorAll('img'));
-const slideCount = slides.length;
-let slideIndex = 0;
+document.addEventListener('DOMContentLoaded', function() {
+  const prevButton = document.querySelector('.prev');
+  const nextButton = document.querySelector('.next');
+  const photoContainer = document.querySelector('.photo');
+  const items = document.querySelectorAll('.slider__item');
+  let currentIndex = 0;
 
-prevButton.addEventListener('click', showPreviousSlide);
-nextButton.addEventListener('click', showNextSlide);
+  function updateSlider() {
+      const offset = -currentIndex * 100;
+      photoContainer.style.transform = `translateX(${offset}%)`;
+  }
 
-function showPreviousSlide() {
-  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-  updateSlider();
-}
-
-function showNextSlide() {
-  slideIndex = (slideIndex + 1) % slideCount;
-  updateSlider();
-}
-
-function updateSlider() {
-  slides.forEach((slide, index) => {
-    if (index === slideIndex) {
-      slide.style.display = 'block';
-    } else {
-      slide.style.display = 'none';
-    }
+  prevButton.addEventListener('click', function() {
+      if (currentIndex > 0) {
+          currentIndex--;
+          updateSlider();
+      }
   });
-}
-updateSlider();
+
+  nextButton.addEventListener('click', function() {
+      if (currentIndex < items.length - 1) {
+          currentIndex++;
+          updateSlider();
+      }
+  });
+
+  updateSlider(); 
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  let accHeaders = document.getElementsByClassName("accordion-header");
+
+  for (let i = 0; i < accHeaders.length; i++) {
+    accHeaders[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      let panel = this.nextElementSibling;
+      let icon = this.querySelector(".accordion-icon");
+
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        panel.classList.remove("active");
+        icon.classList.remove("rotate");
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        panel.classList.add("active");
+        icon.classList.add("rotate");
+      }
+    });
+  }
+});
+
+
